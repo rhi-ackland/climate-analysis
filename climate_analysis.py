6136044ed3c7c00d2b41749dcc587dd33e2e2f04
Rhiannon Ackland <rhiannonackland@gmail.com>
2023-10-11 10:52:35 +0100

    Add rainfall processing placeholder

--- climate_analysis.py
+++ climate_analysis.py
@@ -27,3 +27,5 @@ for line in climate_data:
             kelvin = temp_conversion.fahr_to_kelvin(fahr)
 
             print(str(celsius)+", "+str(kelvin))
+
+# TODO(rhi-ackland): Add call to process rainfall
